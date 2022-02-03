f5bc303264612de086bd058e91fba3bd1b5aba7a
ZBreezie <zpmc55@yahoo.com>
2022-02-03 14:53:14 -0500

    added basic pages and ability to swap page states via nav tabs

---
+++ client/src/components/pages/Schedule.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Schedule = () => (
+  <div>
+    <h1>SCHEDULE</h1>
+    <p>
+      Theres no place like wherever you're traveling.
+    </p>
+  </div>
+
+
+);
+
+export default Schedule;
---
+++ client/src/components/pages/Signup.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Signup = () => (
+  <div>
+    <h1>SIGNUP</h1>
+    <p>
+      A form here...and a form here...
+    </p>
+  </div>
+
+
+);
+
+export default Signup;
